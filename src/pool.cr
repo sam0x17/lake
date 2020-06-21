78609332bcf7bf9b10bf6e13984d6b8b917a5a49
Sam Johnson <sam@durosoft.com>
2020-06-20 22:38:01 -0400

    basic API

--- src/pool.cr
+++ src/pool.cr
@@ -1,16 +1,28 @@
 class Pool(T)
   DEFAULT_CAPACITY = 24
 
-  def initialize(capacity = DEFAULT_CAPACITY)
-    @pool = Array(T).new(capacity)
-    @pool_commands = Array
-    @mutex : Mutex
+  def initialize(capacity = DEFAULT_CAPACITY, @factory = ->{ T.new })
+    @mutex = Mutex.new
+    @pool = Array(Tuple(Channel(T ->), T)).new(capacity)
     @cursor = 0
+    @pool.size.times do |i|
+      chan = Channel(T ->).new
+      obj = @factory.call
+      @pool[i] = {chan, obj}
+      spawn do
+        loop do
+          block = chan.receive
+          block.call(obj)
+        end
+      end
+    end
   end
 
-  def borrow(&block : T ->)
-    @mutex.synchronize do
-      return 
-    end
+  def dip(&block : T ->)
+    spawn { dip_sync(block) }
+  end
+
+  def dip_sync(&block : T ->)
+    mutex.synchronize { @pool[(@cursor += 1) % @pool.size].first.send(block) }
   end
 end
